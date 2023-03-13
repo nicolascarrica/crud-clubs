6092f7590a46d41c6561c4021c869b743924ff3a
nicolascarrica <102767489+nicolascarrica@users.noreply.github.com>
2023-03-13 15:05:43 -0300

    add dependencies
    This project depends on Express.js, Handlebars, Multer, and Nodemon. It also uses ESLint to maintain cleaner code

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: 'airbnb-baseslint:recommended',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+  },
+  rules: {
+  },
+};
